9079b612d8c4edbb8cbe6d94b57923ebc54c7f43
Eliudelima <eliudeangelo@gmail.com>
2020-09-15 20:36:29 -0300

    1 file modified, 4 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { HomeComponent } from './home/home.component';
+import { GitComponent } from './git/git.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, HomeComponent, GitComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
