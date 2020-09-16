d48d9226a0435c1ee50604a5cd50a7c88be71df8
Eliudelima <eliudeangelo@gmail.com>
2020-09-16 19:46:33 -0300

    1 file modified

--- src/app/cursos.ts
+++ src/app/cursos.ts
@@ -21,42 +21,42 @@ export const cursos = [
   },
   {
     title:"3º Semestre (2019)",
-    semestre: "1ºCiclo",
-    semestrea: "Inglês - 6,0",
-    semestreb: "Bases da Internet - 9.6 ",
-    semestrec: "Criação de Conteúdo na Web - 7.9 ",
-    semestred: "Design Digital - 9,2 ",
-    semestree: "Padrões de Projeto de Sítios Internet I - 6.0",
-    semestref: "Fundamentos de Matemática Elementar - 7,0" 
+    semestre: "Banco de Dados e Internet I - 6.7",
+    semestrea: "Engenharia de Software para WEB -  8.5",
+    semestreb: "Inglês III - 6.7 ",
+    semestrec: "Servidores e seus Sistemas Operacionais -  6.5  ",
+    semestred: "Programação de Sítios Internet - 6.0  ",
+    semestree: "Acessibilidade - 6.9",
+    semestref: "Estatística - 6.5  " 
   },
   {
     title:"4º Semestre (2019)",
-    semestre: "1ºCiclo",
-    semestrea: "Inglês - 6,0",
-    semestreb: "Bases da Internet - 9.6 ",
-    semestrec: "Criação de Conteúdo na Web - 7.9 ",
-    semestred: "Design Digital - 9,2 ",
-    semestree: "Padrões de Projeto de Sítios Internet I - 6.0",
-    semestref: "Fundamentos de Matemática Elementar - 7,0" 
+    semestre: "Prática de Gestão de Projetos - 6.0 ",
+    semestrea: "Banco de Dados e Internet II - 7.5 ",
+    semestreb: "Desenvolvimento para Servidores I -  6.0",
+    semestrec: "Inglês IV - 6.4  ",
+    semestred: "Segurança em Sistemas para Internet -  8.3   ",
+    semestree: "Projeto de Navegação e Interação - 7.0 ",
+    semestref: "Tópicos Especiais em Sistemas para Internet I -  8.5 " 
   },
   {
     title:"5º Semestre (2020)",
-    semestre: "1ºCiclo",
-    semestrea: "Inglês - 6,0",
-    semestreb: "Bases da Internet - 9.6 ",
-    semestrec: "Criação de Conteúdo na Web - 7.9 ",
-    semestred: "Design Digital - 9,2 ",
-    semestree: "Padrões de Projeto de Sítios Internet I - 6.0",
-    semestref: "Fundamentos de Matemática Elementar - 7,0" 
+    semestre: "Negócios e Marketing e Eletrônicos - 7.0 ",
+    semestrea: "Desenvolvimento para Servidores II - Em Curso",
+    semestreb: "Inglês V - 9.5 ",
+    semestrec: "Projeto de Prototipagem e Testes de Usabilidade - 7.7  ",
+    semestred: "Desenvolvimento para Dispositivos Móveis I - 7.8  ",
+    semestree: "Tópicos Especiais em Sistemas para Internet II - Em Curso",
+    semestref: "Projeto de Trabalho de Graduação em Sistemas para Internet I - 6.5 " 
   },
   {
     title:"6º Semestre - Em Curso",
-    semestre: "1ºCiclo",
-    semestrea: "Inglês - 6,0",
-    semestreb: "Bases da Internet - 9.6 ",
-    semestrec: "Criação de Conteúdo na Web - 7.9 ",
-    semestred: "Design Digital - 9,2 ",
-    semestree: "Padrões de Projeto de Sítios Internet I - 6.0",
-    semestref: "Fundamentos de Matemática Elementar - 7,0" 
+    semestre: "Criação de Empresas para Internet ",
+    semestrea: "Inglês VI",
+    semestreb: "Arquitetura Orientada a Serviços",
+    semestrec: "Desenvolvimento para Dispositivos Móveis II ",
+    semestred: "Tópicos Especiais em Sistemas para Internet III ",
+    semestree: "Trabalho de Graduação em Sistemas para a Internet",
+    semestref: "Estágio Supervisionado em Sistemas para Internet - Aprovado" 
   }
 ]
