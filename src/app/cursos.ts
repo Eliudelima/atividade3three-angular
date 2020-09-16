9f533a8fa46787e13ce6f67ab0e3662b390f8112
Eliudelima <eliudeangelo@gmail.com>
2020-09-16 19:24:24 -0300

    2 files modified

--- src/app/cursos.ts
+++ src/app/cursos.ts
@@ -1,58 +1,62 @@
 export const cursos = [
   {
-    title:"1ºSemestre (2018)",
-    head: ['Nome', 'Idade', 'Status da Pessoa'],
-    body: [
-      {
-        nome: 'Jonathan',
-        idade: 22,
-        status: 'Pendente'
-      },
-      {
-        nome: 'Jonathan',
-        idade: 22,
-        status: 'Pendente'
-      }
-    ]
+    title:"1º Semestre (2018)",
+    semestre: "Algoritmos e Lógica de Programação - 6,4", 
+    semestrea: "Inglês - 6,0",
+    semestreb: "Bases da Internet - 9,6 ",
+    semestrec: "Criação de Conteúdo na Web - 7,9 ",
+    semestred: "Design Digital - 9,2 ",
+    semestree: "Padrões de Projeto de Sítios Internet I - 6,0",
+    semestref: "Fundamentos de Matemática Elementar - 7,0"    
   },
   {
-    title:"Desenvolvimento para Servidores II",
-    semestre: "1ºCiclo",
-    nota: 19
+    title:"2º Semestre (2018)",
+    semestre: "Legislação Aplicada à Internet - 8,0 ",
+    semestrea: "Estruturas de Dados - 6,8",
+    semestreb: "Inglês II - 6,7  ",
+    semestrec: "Redes e Internet - 6,0 ",
+    semestred: "Padrões de Projeto de Sítios Internet II - 6,0  ",
+    semestree: "Matemática Discreta - 7,7"
+    
   },
   {
-    title:"Inglês VI",
+    title:"3º Semestre (2019)",
     semestre: "1ºCiclo",
-    nota: 19
+    semestrea: "Inglês - 6,0",
+    semestreb: "Bases da Internet - 9.6 ",
+    semestrec: "Criação de Conteúdo na Web - 7.9 ",
+    semestred: "Design Digital - 9,2 ",
+    semestree: "Padrões de Projeto de Sítios Internet I - 6.0",
+    semestref: "Fundamentos de Matemática Elementar - 7,0" 
   },
   {
-    title:"Arquitetura Orientada a Serviços",
+    title:"4º Semestre (2019)",
     semestre: "1ºCiclo",
-    nota: 19
+    semestrea: "Inglês - 6,0",
+    semestreb: "Bases da Internet - 9.6 ",
+    semestrec: "Criação de Conteúdo na Web - 7.9 ",
+    semestred: "Design Digital - 9,2 ",
+    semestree: "Padrões de Projeto de Sítios Internet I - 6.0",
+    semestref: "Fundamentos de Matemática Elementar - 7,0" 
   },
   {
-    title:"Desenvolvimento para Dispositivos Móveis II",
+    title:"5º Semestre (2020)",
     semestre: "1ºCiclo",
-    nota: 19
+    semestrea: "Inglês - 6,0",
+    semestreb: "Bases da Internet - 9.6 ",
+    semestrec: "Criação de Conteúdo na Web - 7.9 ",
+    semestred: "Design Digital - 9,2 ",
+    semestree: "Padrões de Projeto de Sítios Internet I - 6.0",
+    semestref: "Fundamentos de Matemática Elementar - 7,0" 
   },
   {
-    title:"Projeto de Encontrabilidade",
+    title:"6º Semestre - Em Curso",
     semestre: "1ºCiclo",
-    nota: 19
-  },
-  {
-    title:"Tópicos Especiais em Sistemas para Internet II",
-    semestre: "1ºCiclo",
-    nota: 19
-  },
-   {
-    title:"Tópicos Especiais em Sistemas para Internet III",
-    semestre: "1ºCiclo",
-    nota: 19
-  },
-   {
-    title:"Projeto de Trabalho de Graduação em Sistemas para Internet II ",
-    semestre: "1ºCiclo",
-    nota: 19
+    semestrea: "Inglês - 6,0",
+    semestreb: "Bases da Internet - 9.6 ",
+    semestrec: "Criação de Conteúdo na Web - 7.9 ",
+    semestred: "Design Digital - 9,2 ",
+    semestree: "Padrões de Projeto de Sítios Internet I - 6.0",
+    semestref: "Fundamentos de Matemática Elementar - 7,0" 
   }
 ]
