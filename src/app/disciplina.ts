824447f69b81957ad202c3ad0411cfcb1d36d59a
Eliudelima <eliudeangelo@gmail.com>
2020-09-16 21:58:20 -0300

    3 files modified

--- src/app/disciplina.ts
+++ src/app/disciplina.ts
@@ -1,56 +1,183 @@
 export const disciplina = [
   {
-    title:"Criação de Empresas para Internet",
-    ementa: "O planejamento de um projeto de negócios para Internet. Implantação e implementação de projetos de negócios. Administração de custos e dos prazos de implantação em projetos. Identificação de oportunidades de investimentos. Analise de mercado. Desenvolvimento e implementação de negócios. Finanças de um projeto de Investimentos. Análise de sensibilidade. Empreendedorismo e inovação: liderança estratégica. Administração participativa e os novos modelos de administração. Principais funções administrativas e respectivos sistemas de informação. Mecanismos e procedimentos para criação de empresas. Criação de empresas focadas na Internet. Computação em Nuvem - estratégias e modelos",
-    dia: "Terça-Feira",
-    horario: 19 
+    title:"Algoritmos e lógica de programação",
+    ementa: "Conceitos básicos sobre algoritmos e métodos para sua construção. Tipos de dados e variáveis. Álgebra booleana. Estruturas fundamentais de programas: sequencial, condicional e com repetição. Funções. Variáveis compostas homogêneas: vetores e matrizes.",
+    objetivo: "Iniciar a formação em programação de computadores por meio de uma linguagem facilitadora do aprendizado de algoritmos, preferencialmente alguma uma linguagem de amplo emprego no mercado"
+   
   },
   {
-     title:"Desenvolvimento para Servidores II",
-    ementa: "Uso de algum ambiente de desenvolvimento (framework) em plataforma proprietária: configuração do ambiente, emulação de dispositivos móveis nos computadores pessoais,  interface gráfica, serviços disponíveis, banco de dados nos dispositivos. Desenvolvimento de aplicativos com uso de frameworks",
-    dia: "Quinta- Feira",
-    horario: 19
+    title:"Bases da Internet",
+    ementa: "Introdução a Internet. Organizações que viabilizam a Internet e a web. Busca de informações. Email, Listas de Email, e Comunicação. Rich Media. Introdução a Redes de computadores. Métodos de compartilhamento de informações. Segurança. Considerações éticas e legais com uso da Internet.",
+    objetivo: "Fornecer aos estudantes os conhecimentos básicos sobre a Internet. Quem e como a utiliza. Abordar tópicos para permitir aos estudantes desenvolverem habilidades básicas: email, blogs, navegadores, pesquisa, feeds, FTP, downloads, rich media e social media. Explorar vários métodos de comunicação na Web."
   },
   {
-    title:"Inglês VI",
-    ementa: "Aprimoramento da compreensão e produção oral e escrita por meio da integração das habilidades linguístico-comunicativas. Ênfase na oralidade, atendendo às especificidades acadêmico-profissionais da área e abordando aspectos socioculturais da língua inglesa.",
-    dia: "Terça-feira",
-    horario: 21
+    title:"Criação de conteúdo na web",
+    ementa: "Teoria. Adequação comunicativa em diferentes situações de interação. Planejamento de produção de conteúdo textual. Escrita e edição de conteúdo para Web",
+    objetivo: "Criar conteúdos textuais para páginas da Internet."
   },
   {
-    title:"Arquitetura Orientada a Serviços",
-    ementa: "Construção de aplicações por meio da utilização de arquitetura orientada a serviços – SOA (Service-Oriented Architecture) e das principais tecnologias Web Services",
-    dia: "Quarta-feira",
-    horario: 19
+    title:"Design digital",
+    ementa: "Apresentar as bases do design gráfico de produção de sítios Internet com emprego de softwares.",
+    objetivo: "Projetar mock-ups (cenas) usando o software de design digital, em seguida implementar o projeto como um sítio Internet."
   },
   {
-    title:"Desenvolvimento para Dispositivos Móveis II",
-    ementa: "Use yous illusion I",
-    dia: "Quinta-feira",
-    horario: 19
+    title:"Fundamentos de matemática elementar",
+    ementa: " Conjuntos Numéricos. Regras de Três. Frações. Porcentagem. Potenciação e Notação Científica. Radiciação. Logaritmos. Polinômios. Fatoração e Produtos Notáveis. Matrizes. Equações e inequações do 1o. e 2o. Graus.",
+    objetivo: "Discutir os conceitos básicos de matemática"
   },
   {
-    title:"Projeto de Encontrabilidade",
-    ementa: "Teoria Geral. Marcação para encontrabilidade. Microformatos. Conteúdo. Rede Social. Estratégias Server-Side. Como evitar armadilhas de encontrabilidade. Pesquisas. Análises",
-    dia: "Segunda-feira",
-    horario: 19
+    title:"Inglês",
+    ementa: "Introdução à compreensão e produção oral e escrita por meio da integração das habilidades linguístico-comunicativas. Ênfase na oralidade, atendendo às especificidades acadêmico-profissionais da área e abordando aspectos socioculturais da língua inglesa.",
+    objetivo: "Apresentar os recursos da Língua inglesa objetivando desenvolver as quatro habilidades, reading, listening, writing e speaking. Apoiar as atividades de construção de blogs e sítios Internet previstas para o semestre."
   },
   {
-    title:"Tópicos Especiais em Sistemas para Internet II",
-    ementa: "Recursos e aplicações da tecnologia escolhida",
-    dia: "Sexta-feira",
-    horario: 19
+    title:"Leitura e produção de textos",
+    ementa: "A comunicação na atualidade. Coesão e coerência textuais. Uso e adequação da linguagem. Estratégias de leitura de texto. Produção de texto: resumo acadêmico, resenha crítica, relatório, artigo científico.",
+    objetivo: "Ampliar o domínio da comunicação escrita e oral, por meio da aprendizagem e produção de textos existentes no âmbito acadêmico e tecnológico."
+  },
+  {
+    title:"Padrões de projeto de sítios Internet I ",
+    ementa: "Padrões de projeto para Web. Sintaxe do HTML. Elementos básicos dos conceitos de Web Design. HTML doctypes. Validação do HTML. Boas práticas com HTML e CSS: O CSS, seletores e propriedades. Formulários e estrutura de sítios Internet.",
+    objetivo: "Apresentar os fundamentos do HTML (Hypertext Markup Language) com ênfase na semântica do uso dos elementos e nos benefícios do emprego de padrões de codificação. Demonstrar uso do CSS (Cascading Style Sheets) para separar conteúdo e apresentação, de modo a aumentar velocidade do desenvolvimento e as habilidades de design"
+  },
+  {
+    title:"Estruturas de dados",
+    ementa: " Revisão dos conceitos básicos de tipos abstratos de dados. Pilhas, filas, alocação dinâmica, recursividade, listas encadeadas, tabelas de espalhamento e árvores. Aplicações das estruturas de dados em problemas computacionais.",
+     objetivo: "Aprofundar conhecimentos sobre criação e manipulação de tipos abstratos de dados: listas, pilhas, filas e árvores."
+  },
+  {
+    title:"Legislação aplicada à Internet ",
+    ementa: "Introdução ao Direito. Estudo dos impactos na sociedade trazidos pelo computador. Estudo dos princípios éticos relacionados ao uso da Internet. Noções de perícia forense computacional. Aspectos legais de contrato de serviços de TI. Aspectos legais relacionados à Internet. Garantias legais de acesso à informação (dados abertos). Governos eletrônicos. Aspectos legais quanto a negócios intermediados pela Internet e web.",
+    objetivo: " Discutir os fundamentos do Direito aplicado à Internet."
   },
    {
-    title:"Tópicos Especiais em Sistemas para Internet III",
-    ementa: "Recursos e aplicações da tecnologia escolhida",
-    dia: "Sábado",
-    horario: 13
+    title:"Matemática discreta",
+    ementa: " Lógica formal. Teoria dos conjuntos. Matrizes. Determinantes. Sistemas Lineares. Relações. Funções. Análise Combinatória",
+    objetivo: "Apresentar os fundamentos da matemática aplicada ao contexto do curso."
   },
    {
+    title:"Padrões de projeto de sítios Internet II",
+    ementa: "Revisão dos padrões de projeto para Web. Tópicos CSS. Esquemas de posicionamento. Elementos flutuantes. Formulários HTML. Elementos de HTML avançado. Técnicas avançadas de reposição de imagem. Seletores avançados do CSS. Microformatos. Técnicas de mosaico de imagens. Resolução de Problemas de layout com uso de depuradores apropriados. Design responsivo. Web semântica. Acessibilidade. Internacionalização.",
+    objetivo: "Aprendizado das técnicas avançadas de HTML e CSS para criação de páginas Web de layout sofisticado em conformidade com as diretrizes do W3C. Utilização de microformatos para criar conteúdo semântico e portável. Fornecer ao estudante uma base para criação de sítios acessíveis com aplicação de conceitos do Design Universal para acomodar pessoas com deficiência visual, auditiva, fala, controle motor e distúrbios cognitivos."
+  },
+  {
+    title:"Prática de design",
+    ementa: "Tratamento de imagem para composição de cenas para uso na internet. Aplicações de efeitos digitais. Desenvolvimento de layouts interativos para web. Design, interatividade e ambiente. Design, interatividade e ambiente. Introdução à engenharia de usabilidade.",
+    objetivo: "Apresentar os tratamentos refinados de imagens e animações com emprego de softwares ou do canvas do HTML5. Ênfase na concepção de uso de várias interfaces interativas compatíveis com interoperabilidade de navegadores com objetivo de analisar usabilidade e acessibilidade."
+  },
+  {
+    title:"Redes e Internet",
+    ementa: "Apresentar detalhadamente as tecnologias de redes de interesse da Internet. Discutir otimização da comunicação e transmissão de dados nos sítios da Internet.",
+    objetivo: "Internet e os protocolos - detalhamento e implementação dos principais protocolos Internet (DNS, HTTP, SMTP, POP3, NNTP e HTTPS). Identificação de problemas (troubleshooting). "
+  },
+  {
+    title:"Acessibilidade  ",
+    ementa: "Design universal. Usabilidade. Acessibilidade. Tecnologia assistiva. Acessibilidade no HTML e CSS. Testes de acessibilidade. Adobe PDF acessível. Dispositivos de multimídia acessíveis. Scripting acessível. Acessibilidade e dispositivos móveis.",
+    objetivo: "Aplicar técnicas de codificação em HTML e CSS. Avaliar acessibilidade relacionada a softwares de uso comum, scripts, dispositivos móveis e legendagem de Rich media"
+  },
+  {
+    title:"Banco de dados e Internet I ",
+    ementa: "Conceitos de bases de dados, modelos conceituais e lógicos de dados. Modelo entidaderelacionamento. Modelo de dados relacional. Teoria relacional. Restrições de integridade. Linguagem SQL (Structured Query Language). Projeto de banco de dados no modelo relacional. Normalização.",
+    objetivo: "Apresentar os conceitos fundamentais sobre banco de dados e modelagem de dados."
+  },
+  {
+    title:"Engenharia de software para web ",
+    ementa: "Visão Geral da Engenharia de Software – papel evolutivo, paradigmas, ciclo de desenvolvimento. O processo de desenvolvimento: características e processo de desenvolvimento para aplicações Web. Linguagens gráficas para representação dos modelos. Metodologias ágeis. Mapeamento para implementação. Integração do sistema. Testes: planejamento e tipos. Manutenção. Documentação.",
+    objetivo: "Apresentar uma visão ampla e contemporânea da Engenharia de software com ênfase nas metodologias aplicadas ao desenvolvimento para Web"
+  },
+  {
+    title:"Estatística ",
+    ementa: "Conceitos estatísticos. Gráficos e tabelas. Distribuição de frequência. Medidas de posição. Medidas de dispersão. Medidas separatrizes. Medidas de assimetria e curtose. Diagrama de dispersão e correlação. Regressão Linear Simples. Coeficiente de correlação. Probabilidade. Distribuições de probabilidade: variável aleatória discreta e contínua.",
+    objetivo: "Apresentar Estatística e como utilizá-la para modelar problemas."
+  },
+  {
+    title:"Programação de sítios Internet",
+    ementa: "Apresentar aos estudantes conceitos de programação por meio de ECMAScript (European Computer Manufacturers Association - ECMA) – JavaScript - e Document Object Model (DOM) scripts. Apresentar a história ECMAScript, e seu papel em sítios Internet contemporâneos e aplicações Web. Descrever como Scripts básicos e intermediários são criados para resolver problemas comuns de interface. Aprender a avaliar as bibliotecas existentes e scripts para que possam tomar decisões sobre sua aplicabilidade em uma dada tarefa. Aprender as melhores práticas, técnicas de DOM Scripting (JavaScript).",
+    objetivo: "História do JavaScript. Características do ECMAScript. Melhores práticas de DOM Scripting. Ambientes de testes de JavaScript. ECMAScript: Estrutura léxica, tipos de dados, variáveis de script, expressões e operadores, estruturas de controle, strings e funções de manipulação de strings, objetos e arrays, funções, expressões regulares, objetos Date, Math e Number. Conceitos de programação. DOM Scripting, lado cliente. O modelo de objetos para documentos. Suporte para DOM. Acesso aos elementos do DOM. Classes DOM e sistema de manuseio de bibliotecas e eventos. Eventos temporizados. Manuseio de formulários (validação). Introdução ao AJAX. Propriedades e métodos do objeto Window. Acessibilidade e dispositivos móveis. Erros comuns em DHTML e segurança básica"
+  },
+  {
+    title:"Servidores e seus sistemas operacionais ",
+    ementa: "Identificação dos componentes para montar um servidor, compatibilidade, detalhes sobre as especificações de cada componente, identificação e correção de problemas. Instalação e configuração de softwares (aplicativos gerais, gerenciadores de bancos de dados e sistemas operacionais). Compreender os conceitos, mecanismos e funcionamento dos Sistemas operacionais modernos. Criação de partições, formatação de discos, instalação de drivers. Virtualização de servidores para a implementação de serviços de gerenciamento de usuários, serviços de comunicação e de armazenamento de dados através da utilização de uma intranet composta por aplicativos da internet. Configuração de aplicações e serviços Web. Migração de Web sites e aplicações. Configuração de sites seguros (Controle de acesso e Autenticação).",
+    objetivo: "Apresentar os recursos tecnológicos empregados em servidores de redes de computadores."
+  },
+  {
+    title:"Banco de dados e Internet II ",
+    ementa: "Tipos de Junções (join). Subconsultas (Subqueries). Criação, implementação e utilização de procedimentos armazenados (stored procedures), gatilhos (triggers), funções, cursores, visões (view). Linguagem de Controle de Dados. Transações. Bancos de dados orientados a objetos. Bancos de dados multimídia.",
+    objetivo: "Apresentar recursos avançados para criação de aplicativos com Banco de dados"
+  },
+  {
     title:"Projeto de Trabalho de Graduação em Sistemas para Internet II ",
     ementa: "Elaboração de portfólios e técnicas profissionais de apresentação",
-    dia: "Sexta-feira",
-    horario: 21
+    objetivo: ""
+  },
+  {
+    title:"Projeto de Trabalho de Graduação em Sistemas para Internet II ",
+    ementa: "Elaboração de portfólios e técnicas profissionais de apresentação",
+    objetivo: ""
+  },
+  {
+    title:"Projeto de Trabalho de Graduação em Sistemas para Internet II ",
+    ementa: "Elaboração de portfólios e técnicas profissionais de apresentação",
+    objetivo: ""
+  },
+  {
+    title:"Projeto de Trabalho de Graduação em Sistemas para Internet II ",
+    ementa: "Elaboração de portfólios e técnicas profissionais de apresentação",
+    objetivo: ""
+  },
+  {
+    title:"Projeto de Trabalho de Graduação em Sistemas para Internet II ",
+    ementa: "Elaboração de portfólios e técnicas profissionais de apresentação",
+    objetivo: ""
+  },
+  {
+    title:"Projeto de Trabalho de Graduação em Sistemas para Internet II ",
+    ementa: "Elaboração de portfólios e técnicas profissionais de apresentação",
+    objetivo: ""
+  },
+  {
+    title:"Projeto de Trabalho de Graduação em Sistemas para Internet II ",
+    ementa: "Elaboração de portfólios e técnicas profissionais de apresentação",
+    objetivo: ""
+  },
+  {
+    title:"Projeto de Trabalho de Graduação em Sistemas para Internet II ",
+    ementa: "Elaboração de portfólios e técnicas profissionais de apresentação",
+    objetivo: ""
+  },
+  {
+    title:"Projeto de Trabalho de Graduação em Sistemas para Internet II ",
+    ementa: "Elaboração de portfólios e técnicas profissionais de apresentação",
+    objetivo: ""
+  },
+  {
+    title:"Projeto de Trabalho de Graduação em Sistemas para Internet II ",
+    ementa: "Elaboração de portfólios e técnicas profissionais de apresentação",
+    objetivo: ""
+  },
+  {
+    title:"Projeto de Trabalho de Graduação em Sistemas para Internet II ",
+    ementa: "Elaboração de portfólios e técnicas profissionais de apresentação",
+    objetivo: ""
+  },
+  {
+    title:"Projeto de Trabalho de Graduação em Sistemas para Internet II ",
+    ementa: "Elaboração de portfólios e técnicas profissionais de apresentação",
+    objetivo: ""
+  },
+  {
+    title:"Projeto de Trabalho de Graduação em Sistemas para Internet II ",
+    ementa: "Elaboração de portfólios e técnicas profissionais de apresentação",
+    objetivo: ""
+  },
+  {
+    title:"Projeto de Trabalho de Graduação em Sistemas para Internet II ",
+    ementa: "Elaboração de portfólios e técnicas profissionais de apresentação",
+    objetivo: ""
+  },
+  {
+    title:"Projeto de Trabalho de Graduação em Sistemas para Internet II ",
+    ementa: "Elaboração de portfólios e técnicas profissionais de apresentação",
+    objetivo: ""
   }
 ]
